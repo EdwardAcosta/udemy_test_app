42c8daff3ccfabb16f8524e97e1f21e5aed0bd4d
Edward Rene Acosta <edwardacosta@Edwards-Air.satx.rr.com>
2019-12-25 16:02:02 -0600

    Udemy test_app

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+ Rails.application.routes.draw do
+  resources :articles
+get 'welcome/home', to: 'welcome#home' 
+get 'welcome/about', to: 'welcome#about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+ 
