f21561a2fae7cbd8a1dd2ddb78f1c82278a6a82b
Edward Rene Acosta <edwardacosta@Edwards-Air.satx.rr.com>
2020-01-28 13:09:53 -0600

    Initial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_022459) do
+ActiveRecord::Schema.define(version: 2020_01_06_034235) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
